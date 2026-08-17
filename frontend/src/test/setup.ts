07401685dd330b8e9dadc5cbbd9c51cebdee84dc
Sander Pettersen <48487730+Pettersen3008@users.noreply.github.com>
2026-08-17 18:35:11 +0200

    test(frontend): establish refactor baseline [FE-15]

--- frontend/src/test/setup.ts
+++ frontend/src/test/setup.ts
@@ -1,8 +1,16 @@
 import "@testing-library/jest-dom/vitest";
 import { cleanup } from "@testing-library/react";
-import { afterEach } from "vitest";
+import { setupServer } from "msw/node";
+import { afterAll, afterEach, beforeAll } from "vitest";
 
-afterEach(cleanup);
+export const server = setupServer();
+
+beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
+afterEach(() => {
+  cleanup();
+  server.resetHandlers();
+});
+afterAll(() => server.close());
 
 if (!HTMLDialogElement.prototype.showModal) {
   HTMLDialogElement.prototype.showModal = function showModal() {
